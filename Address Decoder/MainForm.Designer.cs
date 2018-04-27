aa660d6d74c8675e63aa9e41eee1389d9b9293f1
amoikevin <1033304289@qq.com>
2018-04-27 15:10:26 +0800

    第一次提交

---
+++ Address Decoder/MainForm.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace System
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSrc = new MetroFramework.Controls.MetroTextBox();
+            this.tbThunber = new MetroFramework.Controls.MetroTextBox();
+            this.tbQQ = new MetroFramework.Controls.MetroTextBox();
+            this.tbFlash = new MetroFramework.Controls.MetroTextBox();
+            this.tbTrue = new MetroFramework.Controls.MetroTextBox();
+            this.tbRayFile = new MetroFramework.Controls.MetroTextBox();
+            this.SuspendLayout();
+            // 
+            // tbSrc
+            // 
+            this.tbSrc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbSrc.CustomButton.Image = null;
+            this.tbSrc.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbSrc.CustomButton.Name = "";
+            this.tbSrc.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbSrc.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbSrc.CustomButton.TabIndex = 1;
+            this.tbSrc.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbSrc.CustomButton.UseSelectable = true;
+            this.tbSrc.CustomButton.Visible = false;
+            this.tbSrc.Lines = new string[0];
+            this.tbSrc.Location = new System.Drawing.Point(66, 83);
+            this.tbSrc.MaxLength = 32767;
+            this.tbSrc.Name = "tbSrc";
+            this.tbSrc.PasswordChar = '\0';
+            this.tbSrc.PromptText = "原始地址";
+            this.tbSrc.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbSrc.SelectedText = "";
+            this.tbSrc.SelectionLength = 0;
+            this.tbSrc.SelectionStart = 0;
+            this.tbSrc.ShortcutsEnabled = true;
+            this.tbSrc.ShowClearButton = true;
+            this.tbSrc.Size = new System.Drawing.Size(538, 23);
+            this.tbSrc.TabIndex = 0;
+            this.tbSrc.UseSelectable = true;
+            this.tbSrc.WaterMark = "原始地址";
+            this.tbSrc.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbSrc.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // tbThunber
+            // 
+            this.tbThunber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbThunber.CustomButton.Image = null;
+            this.tbThunber.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbThunber.CustomButton.Name = "";
+            this.tbThunber.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbThunber.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbThunber.CustomButton.TabIndex = 1;
+            this.tbThunber.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbThunber.CustomButton.UseSelectable = true;
+            this.tbThunber.CustomButton.Visible = false;
+            this.tbThunber.DisplayIcon = true;
+            this.tbThunber.Lines = new string[0];
+            this.tbThunber.Location = new System.Drawing.Point(66, 161);
+            this.tbThunber.MaxLength = 32767;
+            this.tbThunber.Name = "tbThunber";
+            this.tbThunber.PasswordChar = '\0';
+            this.tbThunber.PromptText = "迅雷地址";
+            this.tbThunber.ReadOnly = true;
+            this.tbThunber.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbThunber.SelectedText = "";
+            this.tbThunber.SelectionLength = 0;
+            this.tbThunber.SelectionStart = 0;
+            this.tbThunber.ShortcutsEnabled = true;
+            this.tbThunber.Size = new System.Drawing.Size(538, 23);
+            this.tbThunber.TabIndex = 2;
+            this.tbThunber.UseSelectable = true;
+            this.tbThunber.WaterMark = "迅雷地址";
+            this.tbThunber.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbThunber.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // tbQQ
+            // 
+            this.tbQQ.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbQQ.CustomButton.Image = null;
+            this.tbQQ.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbQQ.CustomButton.Name = "";
+            this.tbQQ.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbQQ.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbQQ.CustomButton.TabIndex = 1;
+            this.tbQQ.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbQQ.CustomButton.UseSelectable = true;
+            this.tbQQ.CustomButton.Visible = false;
+            this.tbQQ.DisplayIcon = true;
+            this.tbQQ.Lines = new string[0];
+            this.tbQQ.Location = new System.Drawing.Point(66, 200);
+            this.tbQQ.MaxLength = 32767;
+            this.tbQQ.Name = "tbQQ";
+            this.tbQQ.PasswordChar = '\0';
+            this.tbQQ.PromptText = "旋风地址";
+            this.tbQQ.ReadOnly = true;
+            this.tbQQ.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbQQ.SelectedText = "";
+            this.tbQQ.SelectionLength = 0;
+            this.tbQQ.SelectionStart = 0;
+            this.tbQQ.ShortcutsEnabled = true;
+            this.tbQQ.Size = new System.Drawing.Size(538, 23);
+            this.tbQQ.TabIndex = 3;
+            this.tbQQ.UseSelectable = true;
+            this.tbQQ.WaterMark = "旋风地址";
+            this.tbQQ.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbQQ.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // tbFlash
+            // 
+            this.tbFlash.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbFlash.CustomButton.Image = null;
+            this.tbFlash.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbFlash.CustomButton.Name = "";
+            this.tbFlash.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbFlash.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbFlash.CustomButton.TabIndex = 1;
+            this.tbFlash.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbFlash.CustomButton.UseSelectable = true;
+            this.tbFlash.CustomButton.Visible = false;
+            this.tbFlash.DisplayIcon = true;
+            this.tbFlash.Lines = new string[0];
+            this.tbFlash.Location = new System.Drawing.Point(66, 239);
+            this.tbFlash.MaxLength = 32767;
+            this.tbFlash.Name = "tbFlash";
+            this.tbFlash.PasswordChar = '\0';
+            this.tbFlash.PromptText = "快车地址";
+            this.tbFlash.ReadOnly = true;
+            this.tbFlash.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbFlash.SelectedText = "";
+            this.tbFlash.SelectionLength = 0;
+            this.tbFlash.SelectionStart = 0;
+            this.tbFlash.ShortcutsEnabled = true;
+            this.tbFlash.Size = new System.Drawing.Size(538, 23);
+            this.tbFlash.TabIndex = 4;
+            this.tbFlash.UseSelectable = true;
+            this.tbFlash.WaterMark = "快车地址";
+            this.tbFlash.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbFlash.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // tbTrue
+            // 
+            this.tbTrue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbTrue.CustomButton.Image = null;
+            this.tbTrue.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbTrue.CustomButton.Name = "";
+            this.tbTrue.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbTrue.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbTrue.CustomButton.TabIndex = 1;
+            this.tbTrue.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbTrue.CustomButton.UseSelectable = true;
+            this.tbTrue.CustomButton.Visible = false;
+            this.tbTrue.DisplayIcon = true;
+            this.tbTrue.Lines = new string[0];
+            this.tbTrue.Location = new System.Drawing.Point(66, 122);
+            this.tbTrue.MaxLength = 32767;
+            this.tbTrue.Name = "tbTrue";
+            this.tbTrue.PasswordChar = '\0';
+            this.tbTrue.PromptText = "真实地址";
+            this.tbTrue.ReadOnly = true;
+            this.tbTrue.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbTrue.SelectedText = "";
+            this.tbTrue.SelectionLength = 0;
+            this.tbTrue.SelectionStart = 0;
+            this.tbTrue.ShortcutsEnabled = true;
+            this.tbTrue.Size = new System.Drawing.Size(538, 23);
+            this.tbTrue.TabIndex = 1;
+            this.tbTrue.UseSelectable = true;
+            this.tbTrue.WaterMark = "真实地址";
+            this.tbTrue.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbTrue.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // tbRayFile
+            // 
+            this.tbRayFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            // 
+            // 
+            // 
+            this.tbRayFile.CustomButton.Image = null;
+            this.tbRayFile.CustomButton.Location = new System.Drawing.Point(516, 1);
+            this.tbRayFile.CustomButton.Name = "";
+            this.tbRayFile.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tbRayFile.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.tbRayFile.CustomButton.TabIndex = 1;
+            this.tbRayFile.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.tbRayFile.CustomButton.UseSelectable = true;
+            this.tbRayFile.CustomButton.Visible = false;
+            this.tbRayFile.DisplayIcon = true;
+            this.tbRayFile.Lines = new string[0];
+            this.tbRayFile.Location = new System.Drawing.Point(66, 278);
+            this.tbRayFile.MaxLength = 32767;
+            this.tbRayFile.Name = "tbRayFile";
+            this.tbRayFile.PasswordChar = '\0';
+            this.tbRayFile.PromptText = "RayFile 地址";
+            this.tbRayFile.ReadOnly = true;
+            this.tbRayFile.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.tbRayFile.SelectedText = "";
+            this.tbRayFile.SelectionLength = 0;
+            this.tbRayFile.SelectionStart = 0;
+            this.tbRayFile.ShortcutsEnabled = true;
+            this.tbRayFile.Size = new System.Drawing.Size(538, 23);
+            this.tbRayFile.TabIndex = 5;
+            this.tbRayFile.UseSelectable = true;
+            this.tbRayFile.WaterMark = "RayFile 地址";
+            this.tbRayFile.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.tbRayFile.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 342);
+            this.Controls.Add(this.tbRayFile);
+            this.Controls.Add(this.tbTrue);
+            this.Controls.Add(this.tbFlash);
+            this.Controls.Add(this.tbQQ);
+            this.Controls.Add(this.tbThunber);
+            this.Controls.Add(this.tbSrc);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 800);
+            this.Name = "MainForm";
+            this.Text = "下载地址链接转换工具";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox tbSrc;
+        private MetroFramework.Controls.MetroTextBox tbThunber;
+        private MetroFramework.Controls.MetroTextBox tbQQ;
+        private MetroFramework.Controls.MetroTextBox tbFlash;
+        private MetroFramework.Controls.MetroTextBox tbTrue;
+        private MetroFramework.Controls.MetroTextBox tbRayFile;
+    }
+}
+
---
+++ Address Decoder/MainForm.cs
@@ -0,0 +1,251 @@
+﻿#region
+
+using System.Net;
+using System.Reactive.Linq;
+using System.Text;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using static System.StringComparison;
+
+#endregion
+
+namespace System
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Observable.FromEventPattern(h => tbSrc.GotFocus += h, h => tbSrc.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(300)).ObserveOn(this).Subscribe(obj => tbSrc.SelectAll());
+            Observable.FromEventPattern(h => tbTrue.GotFocus += h, h => tbTrue.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).ObserveOn(this).Subscribe(obj => SelectAll(tbTrue));
+            Observable.FromEventPattern(h => tbThunber.GotFocus += h, h => tbThunber.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).ObserveOn(this).Subscribe(obj => SelectAll(tbThunber));
+            Observable.FromEventPattern(h => tbQQ.GotFocus += h, h => tbQQ.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).ObserveOn(this).Subscribe(obj => SelectAll(tbQQ));
+            Observable.FromEventPattern(h => tbFlash.GotFocus += h, h => tbFlash.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).ObserveOn(this).Subscribe(obj => SelectAll(tbFlash));
+            Observable.FromEventPattern(h => tbRayFile.GotFocus += h, h => tbRayFile.GotFocus -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).ObserveOn(this).Subscribe(obj => SelectAll(tbRayFile));
+
+            GotFocus += (sender, e) => tbSrc.Focus();
+
+            Observable.FromEventPattern(h => tbSrc.TextChanged += h, h => tbSrc.TextChanged -= h)
+                .Throttle(TimeSpan.FromMilliseconds(800)).Select(obj => tbSrc.Text).DistinctUntilChanged().ObserveOn(this).Subscribe(OnAddressChanged);
+        }
+
+        private void Clear(string value = default(string))
+        {
+            tbTrue.Text = value;
+            tbThunber.Text = value;
+            tbQQ.Text = value;
+            tbFlash.Text = value;
+            tbRayFile.Text = value;
+        }
+
+        private void Decode4Flashget(string address)
+        {
+            tbFlash.Text = address;
+            try
+            {
+                address = address.Substring(11);
+                var base64 = Convert.FromBase64String(address);
+                address = Encoding.Default.GetString(base64);
+                var index = address.IndexOf('&');
+                if (index > -1)
+                {
+                    address = address.Substring(0, index);
+                }
+
+                if (address.StartsWith("[FLASHGET]") && address.EndsWith("[FLASHGET]"))
+                {
+                    address = address.Substring(10, address.Length - 20);
+                    tbTrue.Text = address;
+                    tbThunber.Text = Encode4Thunder(address);
+                    tbQQ.Text = Encode4QQ(address);
+                    tbRayFile.Text = Encode4RayFile(address);
+                }
+
+                return;
+            }
+            catch
+            {
+            }
+
+            Clear("无效的快车地址");
+        }
+
+        private void Decode4QQ(string address)
+        {
+            tbQQ.Text = address;
+            try
+            {
+                address = address.Substring(7);
+                var base64 = Convert.FromBase64String(address);
+                address = Encoding.Default.GetString(base64);
+                tbTrue.Text = address;
+                tbThunber.Text = Encode4Thunder(address);
+                tbFlash.Text = Encode4Flashget(address);
+                tbRayFile.Text = Encode4RayFile(address);
+
+                return;
+            }
+            catch
+            {
+            }
+
+            Clear("无效的旋风地址");
+        }
+
+        private void Decode4RayFile(string address)
+        {
+            tbRayFile.Text = address;
+            try
+            {
+                address = address.Substring(9);
+                var base64 = Convert.FromBase64String(address);
+                address = Encoding.Default.GetString(base64);
+                var index = address.IndexOf('|');
+                if (index > -1)
+                {
+                    address = address.Substring(0, index);
+                }
+
+                address = WebUtility.UrlDecode(address);
+                index = address.IndexOf("://", InvariantCultureIgnoreCase);
+                if (index == -1)
+                    address = $"http://{address}";
+
+                tbTrue.Text = address;
+                tbThunber.Text = Encode4Thunder(address);
+                tbQQ.Text = Encode4QQ(address);
+                tbFlash.Text = Encode4Flashget(address);
+
+
+                return;
+            }
+            catch
+            {
+            }
+
+            Clear("无效的RayFile 地址");
+        }
+
+        private void Decode4Thunder(string address)
+        {
+            tbThunber.Text = address;
+            try
+            {
+                address = address.Substring(10);
+                var base64 = Convert.FromBase64String(address);
+                address = Encoding.Default.GetString(base64);
+                if (address.StartsWith("AA") && address.EndsWith("ZZ"))
+                {
+                    address = address.Substring(2, address.Length - 4);
+                    tbTrue.Text = address;
+                    tbQQ.Text = Encode4QQ(address);
+                    tbFlash.Text = Encode4Flashget(address);
+                    tbRayFile.Text = Encode4RayFile(address);
+                }
+
+                return;
+            }
+            catch
+            {
+            }
+
+            Clear("无效的迅雷地址");
+        }
+
+        private string Encode4Flashget(string address)
+        {
+            address = $"[FLASHGET]{address}[FLASHGET]";
+            address = $"flashget://{Convert.ToBase64String(Encoding.Default.GetBytes(address))}";
+            return address;
+        }
+
+        private string Encode4QQ(string address)
+        {
+            address = $"qqdl://{Convert.ToBase64String(Encoding.Default.GetBytes(address))}";
+            return address;
+        }
+
+        private string Encode4RayFile(string address)
+        {
+            address = $"fs2you://{Convert.ToBase64String(Encoding.Default.GetBytes(address))}";
+            return address;
+        }
+
+        private string Encode4Thunder(string address)
+        {
+            address = $"AA{address}ZZ";
+            address = $"thunder://{Convert.ToBase64String(Encoding.Default.GetBytes(address))}";
+            return address;
+        }
+
+        private void OnAddressChanged(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address))
+            {
+                Clear();
+                return;
+            }
+
+            address = WebUtility.UrlDecode(address);
+            if (address.StartsWith("https://", InvariantCultureIgnoreCase) 
+                || address.StartsWith("http://", InvariantCultureIgnoreCase)
+                || address.StartsWith("ftp://", InvariantCultureIgnoreCase)
+                || address.StartsWith("magnet:?", InvariantCultureIgnoreCase)
+                || address.StartsWith("ed2k://", InvariantCultureIgnoreCase))
+            {
+                tbTrue.Text = address;
+                tbThunber.Text = Encode4Thunder(address);
+                tbQQ.Text = Encode4QQ(address);
+                tbFlash.Text = Encode4Flashget(address);
+                tbRayFile.Text = Encode4RayFile(address);
+                return;
+            }
+
+            if (address.StartsWith("thunder://", InvariantCultureIgnoreCase))
+            {
+                Decode4Thunder(address);
+                return;
+            }
+
+            if (address.StartsWith("qqdl://", InvariantCultureIgnoreCase))
+            {
+                Decode4QQ(address);
+                return;
+            }
+
+            if (address.StartsWith("flashget://", InvariantCultureIgnoreCase))
+            {
+                Decode4Flashget(address);
+                return;
+            }
+
+            if (address.StartsWith("fs2you://", InvariantCultureIgnoreCase))
+            {
+                Decode4RayFile(address);
+                return;
+            }
+
+            Clear("无效的地址");
+        }
+
+        private void SelectAll(MetroTextBox tb)
+        {
+            if (string.IsNullOrEmpty(tb.Text)) return;
+            tb.SelectAll();
+//            try
+//            {
+//                Clipboard.SetText(tb.Text, TextDataFormat.UnicodeText);
+//            }
+//            catch
+//            {
+//            }
+        }
+    }
+}
